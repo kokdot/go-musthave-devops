a644f1c0f6ba042db40741abf1daa7d08531ce44
kokdot <kok.74@mail.ru>
2022-12-02 21:54:06 +0500

    first

--- cmd/server/main.go
+++ cmd/server/main.go
@@ -1,3 +1,31 @@
 package main
 
-func main() {}
+import (
+    "fmt"
+    "net/http"
+	"log"
+	"strings"
+	"strconv"
+)
+// http://<АДРЕС_СЕРВЕРА>/update/<ТИП_МЕТРИКИ>/<ИМЯ_МЕТРИКИ>/<ЗНАЧЕНИЕ_МЕТРИКИ>
+type GaugeMap map[string]float64
+
+var guageMap = GaugeMap{}
+func main() {
+
+	http.HandleFunc("/update/", Handler)
+    fmt.Println("Server started at port 8080")
+
+    log.Fatal(http.ListenAndServe("127.0.0.1:8080", nil))
+}
+
+func Handler(w http.ResponseWriter, r *http.Request) {
+	urlPath := r.URL.Path
+	sliceUrlPath := strings.Split(urlPath, "/")
+	if n, err := strconv.ParseFloat(sliceUrlPath[4], 64); err == nil {
+        guageMap[sliceUrlPath[3]] = n
+        }
+	// fmt.Fprintf(w, "Hello, %v, %T", guageMap, guageMap)
+	w.WriteHeader(http.StatusOK)
+}
+
