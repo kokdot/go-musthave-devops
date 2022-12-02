8e8201929072c6b7457f8142329948e418e1df78
kokdot <kok.74@mail.ru>
2022-12-02 22:15:29 +0500

    first2

--- cmd/server/main.go
+++ cmd/server/main.go
@@ -9,8 +9,10 @@ import (
 )
 // http://<АДРЕС_СЕРВЕРА>/update/<ТИП_МЕТРИКИ>/<ИМЯ_МЕТРИКИ>/<ЗНАЧЕНИЕ_МЕТРИКИ>
 type GaugeMap map[string]float64
+type CounterMap map[string][]int
 
 var guageMap = GaugeMap{}
+var counterMap = CounterMap{}
 func main() {
 
 	http.HandleFunc("/update/", Handler)
@@ -22,9 +24,17 @@ func main() {
 func Handler(w http.ResponseWriter, r *http.Request) {
 	urlPath := r.URL.Path
 	sliceURLPath := strings.Split(urlPath, "/")
-	if n, err := strconv.ParseFloat(sliceURLPath[4], 64); err == nil {
-        guageMap[sliceURLPath[3]] = n
+	if sliceURLPath[2] == "Guage" {
+		
+		if n, err := strconv.ParseFloat(sliceURLPath[4], 64); err == nil {
+			guageMap[sliceURLPath[3]] = n
         }
+	}
+	if sliceURLPath[2] == "Counter" {
+		if n, err := strconv.Atoi(sliceURLPath[4]); err == nil {
+			counterMap[sliceURLPath[3]] = append(counterMap[sliceURLPath[3]], n)
+		}
+	}
 	// fmt.Fprintf(w, "Hello, %v, %T", guageMap, guageMap)
 	w.WriteHeader(http.StatusOK)
 }
