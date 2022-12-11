25e11009b653233de78895e14ca73200ed244fa9
kokdot <kok.74@mail.ru>
2022-12-11 13:41:21 +0500

    sixth2

--- cmd/server/main.go
+++ cmd/server/main.go
@@ -16,84 +16,81 @@ import (
 
 var m store.Repo
 
-
-
-
 func main() {
 	var ms = new(store.MemStorage)
 	ms.GaugeMap = make(store.GaugeMap)
 	ms.CounterMap = make(store.CounterMap)
 	m = ms
-    // определяем роутер chi
-    r := chi.NewRouter()
-    // зададим встроенные middleware, чтобы улучшить стабильность приложения
-    r.Use(middleware.RequestID)
-    r.Use(middleware.RealIP)
-    r.Use(middleware.Logger)
-    r.Use(middleware.Recoverer)
-    r.Get("/", GetAll)
-    r.Route("/update", func(r chi.Router) {
-        r.Route("/counter", func(r chi.Router) {
-            r.Route("/{nameData}/{valueData}", func(r chi.Router) {
-                r.Use(PostCounterCtx)
-                r.Post("/", PostUpdateCounter)
-            })
-        })
-        r.Route("/gauge", func(r chi.Router) {
-            r.Route("/{nameData}/{valueData}", func(r chi.Router) {
-                r.Use(PostGaugeCtx)
-                r.Post("/", PostUpdateGauge)
-            })
-        })
-        r.Route("/",func(r chi.Router) {
-            r.Post("/*", func(w http.ResponseWriter, r *http.Request) {
-		        w.Header().Set("content-type", "text/plain; charset=utf-8")
-                w.WriteHeader(http.StatusNotImplemented)
-                fmt.Fprint(w, "line: 52; http.StatusNotImplemented")
-	        })
-        })
-    })
-
-    r.Route("/value", func(r chi.Router) {
-		r.Route("/counter", func(r chi.Router){
-            r.Route("/{nameData}", func(r chi.Router) {
-                r.Use(GetCtx)
-                r.Get("/", GetCounter)
-            })
-        })
-       	r.Route("/gauge", func(r chi.Router){
-            r.Route("/{nameData}", func(r chi.Router) {
-                r.Use(GetCtx)
-                r.Get("/", GetGauge)
-            })
-        })
+	// определяем роутер chi
+	r := chi.NewRouter()
+	// зададим встроенные middleware, чтобы улучшить стабильность приложения
+	r.Use(middleware.RequestID)
+	r.Use(middleware.RealIP)
+	r.Use(middleware.Logger)
+	r.Use(middleware.Recoverer)
+	r.Get("/", GetAll)
+	r.Route("/update", func(r chi.Router) {
+		r.Route("/counter", func(r chi.Router) {
+			r.Route("/{nameData}/{valueData}", func(r chi.Router) {
+				r.Use(PostCounterCtx)
+				r.Post("/", PostUpdateCounter)
+			})
+		})
+		r.Route("/gauge", func(r chi.Router) {
+			r.Route("/{nameData}/{valueData}", func(r chi.Router) {
+				r.Use(PostGaugeCtx)
+				r.Post("/", PostUpdateGauge)
+			})
+		})
+		r.Route("/", func(r chi.Router) {
+			r.Post("/*", func(w http.ResponseWriter, r *http.Request) {
+				w.Header().Set("content-type", "text/plain; charset=utf-8")
+				w.WriteHeader(http.StatusNotImplemented)
+				fmt.Fprint(w, "line: 52; http.StatusNotImplemented")
+			})
+		})
 	})
 
-    log.Fatal(http.ListenAndServe(":8080", r))
+	r.Route("/value", func(r chi.Router) {
+		r.Route("/counter", func(r chi.Router) {
+			r.Route("/{nameData}", func(r chi.Router) {
+				r.Use(GetCtx)
+				r.Get("/", GetCounter)
+			})
+		})
+		r.Route("/gauge", func(r chi.Router) {
+			r.Route("/{nameData}", func(r chi.Router) {
+				r.Use(GetCtx)
+				r.Get("/", GetGauge)
+			})
+		})
+	})
+
+	log.Fatal(http.ListenAndServe(":8080", r))
 }
 
 func PostCounterCtx(next http.Handler) http.Handler {
-    return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
-        var nameData string
-        var valueData int
+	return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
+		var nameData string
+		var valueData int
 
 		nameDataStr := chi.URLParam(r, "nameData")
 		valueDataStr := chi.URLParam(r, "valueData")
 
-        if nameDataStr == "" || valueDataStr == "" {
-            w.Header().Set("content-type", "text/plain; charset=utf-8")
-		    w.WriteHeader(http.StatusNotFound)
-            fmt.Fprint(w, "http.StatusNotFound")
-            return
-        }
-        nameData = nameDataStr
-        valueData, err := strconv.Atoi(valueDataStr)
-        if err != nil {
-            w.Header().Set("content-type", "text/plain; charset=utf-8")
+		if nameDataStr == "" || valueDataStr == "" {
+			w.Header().Set("content-type", "text/plain; charset=utf-8")
+			w.WriteHeader(http.StatusNotFound)
+			fmt.Fprint(w, "http.StatusNotFound")
+			return
+		}
+		nameData = nameDataStr
+		valueData, err := strconv.Atoi(valueDataStr)
+		if err != nil {
+			w.Header().Set("content-type", "text/plain; charset=utf-8")
 			w.WriteHeader(http.StatusBadRequest)
-            fmt.Fprint(w, "http.StatusBadRequest")
-            return
-        }
+			fmt.Fprint(w, "http.StatusBadRequest")
+			return
+		}
 
 		ctx := context.WithValue(r.Context(), "nameData", nameData)
 		ctx = context.WithValue(ctx, "valueData", valueData)
@@ -101,16 +98,16 @@ func PostCounterCtx(next http.Handler) http.Handler {
 	})
 }
 func GetCtx(next http.Handler) http.Handler {
-    return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
-        var nameData string
+	return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
+		var nameData string
 		nameDataStr := chi.URLParam(r, "nameData")
-        if nameDataStr == "" {
-            w.Header().Set("content-type", "text/plain; charset=utf-8")
-		    w.WriteHeader(http.StatusNotFound)
-            fmt.Fprint(w, "line: 115; http.StatusNotFound")
-            return
-        }
-        nameData = nameDataStr
+		if nameDataStr == "" {
+			w.Header().Set("content-type", "text/plain; charset=utf-8")
+			w.WriteHeader(http.StatusNotFound)
+			fmt.Fprint(w, "line: 115; http.StatusNotFound")
+			return
+		}
+		nameData = nameDataStr
 
 		ctx := context.WithValue(r.Context(), "nameData", nameData)
 		next.ServeHTTP(w, r.WithContext(ctx))
@@ -118,27 +115,27 @@ func GetCtx(next http.Handler) http.Handler {
 }
 
 func PostGaugeCtx(next http.Handler) http.Handler {
-    return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
-        var nameData string
-        var valueData float64
+	return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
+		var nameData string
+		var valueData float64
 
 		nameDataStr := chi.URLParam(r, "nameData")
 		valueDataStr := chi.URLParam(r, "valueData")
 
-        if nameDataStr == "" || valueDataStr == "" {
-            w.Header().Set("content-type", "text/plain; charset=utf-8")
-		    w.WriteHeader(http.StatusNotFound)
-            fmt.Fprint(w, "http.StatusNotFound")
-            return
-        }
-        nameData = nameDataStr
-        valueData, err := strconv.ParseFloat(valueDataStr, 64)
-        if err != nil {
-            w.Header().Set("content-type", "text/plain; charset=utf-8")
+		if nameDataStr == "" || valueDataStr == "" {
+			w.Header().Set("content-type", "text/plain; charset=utf-8")
+			w.WriteHeader(http.StatusNotFound)
+			fmt.Fprint(w, "http.StatusNotFound")
+			return
+		}
+		nameData = nameDataStr
+		valueData, err := strconv.ParseFloat(valueDataStr, 64)
+		if err != nil {
+			w.Header().Set("content-type", "text/plain; charset=utf-8")
 			w.WriteHeader(http.StatusBadRequest)
-            fmt.Fprint(w, "http.StatusBadRequest")
-            return
-        }
+			fmt.Fprint(w, "http.StatusBadRequest")
+			return
+		}
 
 		ctx := context.WithValue(r.Context(), "nameData", nameData)
 		ctx = context.WithValue(ctx, "valueData", valueData)
@@ -148,47 +145,47 @@ func PostGaugeCtx(next http.Handler) http.Handler {
 func PostUpdateCounter(w http.ResponseWriter, r *http.Request) {
 	valueData := r.Context().Value("valueData").(int)
 	nameData := r.Context().Value("nameData").(string)
-    m.SaveCounterValue(nameData, store.Counter(valueData))
+	m.SaveCounterValue(nameData, store.Counter(valueData))
 	w.Header().Set("content-type", "text/plain; charset=utf-8")
 	w.WriteHeader(http.StatusOK)
-    fmt.Fprint(w, "http.StatusOK")
+	fmt.Fprint(w, "http.StatusOK")
 }
 func PostUpdateGauge(w http.ResponseWriter, r *http.Request) {
 	valueData := r.Context().Value("valueData").(float64)
 	nameData := r.Context().Value("nameData").(string)
-    m.SaveGaugeValue(nameData, store.Gauge(valueData))
+	m.SaveGaugeValue(nameData, store.Gauge(valueData))
 	w.Header().Set("content-type", "text/plain; charset=utf-8")
 	w.WriteHeader(http.StatusOK)
-    fmt.Fprint(w, "http.StatusOK")
+	fmt.Fprint(w, "http.StatusOK")
 }
 func GetCounter(w http.ResponseWriter, r *http.Request) {
-    nameData := r.Context().Value("nameData").(string)
-    n, err := m.GetCounterValue(nameData)
-    if err != nil {
-        w.Header().Set("content-type", "text/plain; charset=utf-8")
-        w.WriteHeader(http.StatusNotFound)
-        fmt.Fprint(w, "line: 175; http.StatusNotFound")
-    } else {
-        w.Header().Set("content-type", "text/plain; charset=utf-8")
-        w.WriteHeader(http.StatusOK)
-        fmt.Fprintf(w, "%v", n)
-    }
+	nameData := r.Context().Value("nameData").(string)
+	n, err := m.GetCounterValue(nameData)
+	if err != nil {
+		w.Header().Set("content-type", "text/plain; charset=utf-8")
+		w.WriteHeader(http.StatusNotFound)
+		fmt.Fprint(w, "line: 175; http.StatusNotFound")
+	} else {
+		w.Header().Set("content-type", "text/plain; charset=utf-8")
+		w.WriteHeader(http.StatusOK)
+		fmt.Fprintf(w, "%v", n)
+	}
 }
 func GetGauge(w http.ResponseWriter, r *http.Request) {
-    nameData := r.Context().Value("nameData").(string)
-    n, err := m.GetGaugeValue(nameData)
-    if err != nil {
-        w.Header().Set("content-type", "text/plain; charset=utf-8")
-        w.WriteHeader(http.StatusNotFound)
-        fmt.Fprint(w, "line: 188; http.StatusNotFound")
-    } else {
-        w.Header().Set("content-type", "text/plain; charset=utf-8")
-        w.WriteHeader(http.StatusOK)
-        fmt.Fprintf(w, "%v", n)
-    }    
+	nameData := r.Context().Value("nameData").(string)
+	n, err := m.GetGaugeValue(nameData)
+	if err != nil {
+		w.Header().Set("content-type", "text/plain; charset=utf-8")
+		w.WriteHeader(http.StatusNotFound)
+		fmt.Fprint(w, "line: 188; http.StatusNotFound")
+	} else {
+		w.Header().Set("content-type", "text/plain; charset=utf-8")
+		w.WriteHeader(http.StatusOK)
+		fmt.Fprintf(w, "%v", n)
+	}
 }
 func GetAll(w http.ResponseWriter, r *http.Request) {
-    w.Header().Set("content-type", "text/plain; charset=utf-8")
-    w.WriteHeader(http.StatusOK)
-    fmt.Fprintf(w, "%v", m.GetAllValues()) 
+	w.Header().Set("content-type", "text/plain; charset=utf-8")
+	w.WriteHeader(http.StatusOK)
+	fmt.Fprintf(w, "%v", m.GetAllValues())
 }
--- cmd/server/main_test.go
+++ cmd/server/main_test.go
@@ -1,17 +1,16 @@
 package main
 
 import (
+	"fmt"
 	"github.com/stretchr/testify/assert"
+	"io"
 	"net/http"
 	"net/http/httptest"
 	"testing"
-	"fmt"
-	"io"
 
 	"github.com/go-chi/chi/v5"
 	"github.com/go-chi/chi/v5/middleware"
 	"github.com/kokdot/go-musthave-devops/internal/store"
-
 )
 
 func TestHandler(t *testing.T) {
@@ -20,58 +19,58 @@ func TestHandler(t *testing.T) {
 	ms.GaugeMap = make(store.GaugeMap)
 	ms.CounterMap = make(store.CounterMap)
 	m = ms
-    // зададим встроенные middleware, чтобы улучшить стабильность приложения
-    r.Use(middleware.RequestID)
-    r.Use(middleware.RealIP)
-    r.Use(middleware.Logger)
-    r.Use(middleware.Recoverer)
-    r.Get("/", GetAll)
-    r.Route("/update", func(r chi.Router) {
-        r.Route("/counter", func(r chi.Router) {
-            r.Route("/{nameData}/{valueData}", func(r chi.Router) {
-                r.Use(PostCounterCtx)
-                r.Post("/", PostUpdateCounter)
-            })
-        })
-        r.Route("/gauge", func(r chi.Router) {
-            r.Route("/{nameData}/{valueData}", func(r chi.Router) {
-                r.Use(PostGaugeCtx)
-                r.Post("/", PostUpdateGauge)
-            })
-        })
-        r.Route("/",func(r chi.Router) {
-            r.Post("/*", func(w http.ResponseWriter, r *http.Request) {
-		        w.Header().Set("content-type", "text/plain; charset=utf-8")
-                w.WriteHeader(http.StatusNotImplemented)
-                fmt.Fprint(w, "line: 52; http.StatusNotImplemented")
-	        })
-        })
-    })
+	// зададим встроенные middleware, чтобы улучшить стабильность приложения
+	r.Use(middleware.RequestID)
+	r.Use(middleware.RealIP)
+	r.Use(middleware.Logger)
+	r.Use(middleware.Recoverer)
+	r.Get("/", GetAll)
+	r.Route("/update", func(r chi.Router) {
+		r.Route("/counter", func(r chi.Router) {
+			r.Route("/{nameData}/{valueData}", func(r chi.Router) {
+				r.Use(PostCounterCtx)
+				r.Post("/", PostUpdateCounter)
+			})
+		})
+		r.Route("/gauge", func(r chi.Router) {
+			r.Route("/{nameData}/{valueData}", func(r chi.Router) {
+				r.Use(PostGaugeCtx)
+				r.Post("/", PostUpdateGauge)
+			})
+		})
+		r.Route("/", func(r chi.Router) {
+			r.Post("/*", func(w http.ResponseWriter, r *http.Request) {
+				w.Header().Set("content-type", "text/plain; charset=utf-8")
+				w.WriteHeader(http.StatusNotImplemented)
+				fmt.Fprint(w, "line: 52; http.StatusNotImplemented")
+			})
+		})
+	})
 
-    r.Route("/value", func(r chi.Router) {
-		r.Route("/counter", func(r chi.Router){
-            r.Route("/{nameData}", func(r chi.Router) {
-                r.Use(GetCtx)
-                r.Get("/", GetCounter)
-            })
-        })
-       	r.Route("/gauge", func(r chi.Router){
-            r.Route("/{nameData}", func(r chi.Router) {
-                r.Use(GetCtx)
-                r.Get("/", GetGauge)
-            })
-        })
+	r.Route("/value", func(r chi.Router) {
+		r.Route("/counter", func(r chi.Router) {
+			r.Route("/{nameData}", func(r chi.Router) {
+				r.Use(GetCtx)
+				r.Get("/", GetCounter)
+			})
+		})
+		r.Route("/gauge", func(r chi.Router) {
+			r.Route("/{nameData}", func(r chi.Router) {
+				r.Use(GetCtx)
+				r.Get("/", GetGauge)
+			})
+		})
 	})
 
 	type want struct {
 		StatusCode  int
 		contentType string
-		result string
+		result      string
 	}
 	tests := []struct {
-		name string
-		want want
-		url  string
+		name   string
+		want   want
+		url    string
 		method string
 	}{
 		{
@@ -80,7 +79,7 @@ func TestHandler(t *testing.T) {
 				StatusCode:  http.StatusOK,
 				contentType: "text/plain; charset=utf-8",
 			},
-			url: "/update/counter/PollCount/5",
+			url:    "/update/counter/PollCount/5",
 			method: http.MethodPost,
 		},
 		{
@@ -89,7 +88,7 @@ func TestHandler(t *testing.T) {
 				StatusCode:  http.StatusBadRequest,
 				contentType: "text/plain; charset=utf-8",
 			},
-			url: "/update/counter/PollCount/none",
+			url:    "/update/counter/PollCount/none",
 			method: http.MethodPost,
 		},
 		{
@@ -98,7 +97,7 @@ func TestHandler(t *testing.T) {
 				StatusCode:  http.StatusOK,
 				contentType: "text/plain; charset=utf-8",
 			},
-			url: "/update/gauge/Alloc/3.6",
+			url:    "/update/gauge/Alloc/3.6",
 			method: http.MethodPost,
 		},
 		{
@@ -107,7 +106,7 @@ func TestHandler(t *testing.T) {
 				StatusCode:  http.StatusBadRequest,
 				contentType: "text/plain; charset=utf-8",
 			},
-			url: "/update/gauge/Alloc/none",
+			url:    "/update/gauge/Alloc/none",
 			method: http.MethodPost,
 		},
 		{
@@ -116,16 +115,16 @@ func TestHandler(t *testing.T) {
 				StatusCode:  http.StatusNotImplemented,
 				contentType: "text/plain; charset=utf-8",
 			},
-			url: "/update/error/PollCount/100",
+			url:    "/update/error/PollCount/100",
 			method: http.MethodPost,
 		},
 		{
 			name: "default",
 			want: want{
-				StatusCode:  http.StatusMethodNotAllowed             ,
+				StatusCode:  http.StatusMethodNotAllowed,
 				contentType: "text/plain; charset=utf-8",
 			},
-			url: "/",
+			url:    "/",
 			method: http.MethodPost,
 		},
 		{
@@ -133,25 +132,25 @@ func TestHandler(t *testing.T) {
 			want: want{
 				StatusCode:  http.StatusOK,
 				contentType: "text/plain; charset=utf-8",
-				result: "5",
+				result:      "5",
 			},
 			method: http.MethodGet,
-			url: "/value/counter/PollCount",
+			url:    "/value/counter/PollCount",
 		},
 		{
 			name: "get gauge",
 			want: want{
 				StatusCode:  http.StatusOK,
 				contentType: "text/plain; charset=utf-8",
-				result: "3.6",
+				result:      "3.6",
 			},
-			url: "/value/gauge/Alloc",
+			url:    "/value/gauge/Alloc",
 			method: http.MethodGet,
 		},
 	}
 	ts := httptest.NewServer(r)
 	defer ts.Close()
-	
+
 	for _, tt := range tests {
 		t.Run(tt.name, func(t *testing.T) {
 			request := httptest.NewRequest(tt.method, tt.url, nil)
@@ -159,13 +158,13 @@ func TestHandler(t *testing.T) {
 			r.ServeHTTP(w, request)
 			result := w.Result()
 
-			body, err := io.ReadAll(result.Body) 
+			body, err := io.ReadAll(result.Body)
 			result.Body.Close()
 
 			assert.NoError(t, err)
 			assert.Equal(t, tt.want.StatusCode, result.StatusCode)
 			fmt.Println(tt.name)
-			if tt.name != "default"{
+			if tt.name != "default" {
 				assert.Equal(t, tt.want.contentType, result.Header.Get("Content-Type"))
 			}
 			if tt.method == http.MethodGet {
